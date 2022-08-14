49398dc99370828321546937495e79bd37d6cf89
Reptoxx2 <ilya.naik11@gmail.com>
2022-08-14 17:20:21 +0300

    slider fix

---
+++ docroot/js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function (){
+  $('.content__banner-slide').slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    infinite: true,
+    slidesToScroll: 1,
+    autoplaySpeed: 4000,
+  });
+});
+$(document).ready(function (){
+  $('.content__share-slider').slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    infinite: true,
+    slidesToScroll: 1,
+    autoplaySpeed: 4000,
+  });
+});
+$(document).ready(function (){
+  $('.content__features-slider').slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    infinite: true,
+    slidesToScroll: 1,
+    autoplaySpeed: 4000,
+  });
+});
